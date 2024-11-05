5018ab1e3f2d1d0481f0f84e5e398f4d8656cf2d
JiyeonMin <ichbinmin2@gmail.com>
2024-11-05 20:49:11 +0900

    feat : GLTF 확장자 3모델 import하기

--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -1,14 +1,13 @@
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
 
 export const ShowRoom = () => {
-  const obj = useLoader(FBXLoader, "./models/burger/Burger.obj");
-
+  const obj = useLoader(GLTFLoader, "./models/burger/Burger.glb");
   return (
     <>
-      <primitive object={obj} />
+      <primitive object={obj.scene} />
       {/* <mesh
         rotation={[
           THREE.MathUtils.degToRad(45),
