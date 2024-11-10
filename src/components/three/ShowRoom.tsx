f4e92f55fb922f18ac21d1731225c036fc8851cc
JiyeonMin <ichbinmin2@gmail.com>
2024-11-10 13:02:14 +0900

    feat : setLookAt, fitToBox, fitToSphere 테스트

--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -3,12 +3,12 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
 import { useThree } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export const ShowRoom = () => {
   const { raycaster, camera } = useThree();
+  const cameraControlsRef = useRef<CameraControls>(null);
   const gltf = useLoader(GLTFLoader, "./models/burger/Burger.glb");
-
-  console.log("gltf", gltf);
 
   const burgerClick = () => {
     console.log("burger click");
@@ -24,6 +24,19 @@ export const ShowRoom = () => {
       if (firstObj.name === "bun") {
         const mat = firstObj.material as THREE.MeshStandardMaterial;
         mat.color = new THREE.Color("#be945f");
+
+        // cameraControlsRef.current?.setLookAt(
+        //   10,
+        //   10,
+        //   10,
+        //   firstObj.position.x,
+        //   firstObj.position.y,
+        //   firstObj.position.z,
+        //   true
+        // );
+
+        cameraControlsRef.current?.fitToBox(firstObj, true);
+        // cameraControlsRef.current?.fitToSphere(firstObj, true);
       }
     }
   };
@@ -32,6 +45,7 @@ export const ShowRoom = () => {
     <>
       <directionalLight position={[3, 3, 3]} />
       <CameraControls
+        ref={cameraControlsRef}
         enabled={true} // 조건에 따른 컨트롤즈 사용 여부를 결정하는 옵션
         dollyToCursor={true}
         // 커서의 방향으로 줌을 당긴다는 뜻
@@ -42,7 +56,7 @@ export const ShowRoom = () => {
           // console.log("camera.zoom", camera.zoom);
         }}
         minDistance={2} // 줌 인을 하다보면 3D모델링의 절두체에 잘려서 보이는 경우가 있다. 이를 방지하기 위해서 대부분 카메라가 갈수 있는 거리를 한정해놓는다.
-        maxDistance={10} // 줌 아웃을 하다보면 3D모델링과 아주 멀어져서 보이는 경우가 있다. 이를 방지하기 위해서 maxDistance 옵션을 통해 대부분 카메라가 갈수 있는 거리를 한정해놓는다.
+        // maxDistance={10} // 줌 아웃을 하다보면 3D모델링과 아주 멀어져서 보이는 경우가 있다. 이를 방지하기 위해서 maxDistance 옵션을 통해 대부분 카메라가 갈수 있는 거리를 한정해놓는다.
         // infinityDolly={true} // infinityDolly 를 true로 설정하면, 내가 설정한 minDistance와 maxDistance 값을 다 무시하게 된다.
       />
       <primitive object={gltf.scene} onClick={burgerClick} />
